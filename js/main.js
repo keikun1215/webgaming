d57757fcb797cfa2be84e4a694e26edf13932442
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-01-02 23:04:17 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ addEventListener( 'load', () => {
 
 	//変数gameに、あなたはゲームですよ、と教える
 	const game = new Game();
+	
+		//変数yamadaに、あなたは山田先生のスプライト画像ですよ、と教える
+	const yamada = new Sprite( 'img/yamada.png' );
+	//gameに、山田先生のスプライト画像を表示して、とお願いする
+	game.add( yamada );
 
 	const game = new Game( 1136, 640 );
 	
